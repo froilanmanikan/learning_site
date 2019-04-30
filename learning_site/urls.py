a17c53289ba1a9520f1849a772610ba24038bf5c
froilanmanikan <froilanmanikan@gmail.com>
2019-04-30 08:12:48 +0800

    update 04302019

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include,url
+
+from . import views
 
 urlpatterns = [
+	url(r'^courses/',include('courses.urls')),
     path('admin/', admin.site.urls),
+    url(r'^$',views.hello_world),
+
 ]
